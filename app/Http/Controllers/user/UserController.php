227245f54426cff88bf92e9ca99533cdaaa06f4c
mohammad <m.j.malmaling52@gmail.com>
2020-10-01 17:35:07 +0330

    add login with username email and phone number features / register with email and phone number and validation for these.

---
+++ app/Http/Controllers/user/UserController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers\user;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+use App\Http\Controllers\Controller;
+use App\Rules\PhoneNumber;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function filed_type(request $request)
+    {
+        // $validation = $request->validate(['unique_field' => 'required|regex:/(01)[0-9]{9}/']);
+        // $request->session()->reflash('errors', 'Your mobile number not match in our system..!!');
+        // $validator = new validator();
+        // $validator->getMessageBag()->add('password', 'Password wrong');
+
+        // $unique_field = $request->unique_field;
+        // $result = 'hh';
+        // if (is_numeric($unique_field)) {
+        //     //user insert phone number for login
+
+        //     $validation = $request->validate(['unique_field' => ['required',new PhoneNumber]]);
+        //     dd($validation);
+        //     $result = 'you are logig with phone number';
+        // } elseif (filter_var($unique_field, FILTER_VALIDATE_EMAIL)) {
+        //     // user insert email for login
+        //     $result = 'you are logig with email address';
+        // }
+        // return view('welcome');
+    }
+}
---
+++ app/Rules/PhoneNumber.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class PhoneNumber implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if (isset($value)) {
+            $non_cahrs = [' '];
+            $value = str_replace($non_cahrs, '', $value);
+            // dd($value);
+            if (strlen($value) == 11) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'شماره مبایل وارد شده صحیح نمیباشد.';
+    }
+}
