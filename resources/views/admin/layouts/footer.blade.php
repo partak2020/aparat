e7095463a32cd1ff784eb1646b7eda48df3b1ca1
Hadise Danedaran <hdanedaran@yahoo.com>
2020-10-31 23:24:51 +0330

    seprate footer layout from dashboardlayout to organize them

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="sticky-footer">
+    <section class="section-padding">
+        <div class="container">
+            <div class="row">
+                <p>زیر نظر گروه آموزشی سیتکو
+                    <br> partak.group2020@gmail.com
+                </p>
+            </div>
+        </div>
+    </section>
+</footer>
