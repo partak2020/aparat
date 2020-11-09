3d70bb3294a5b997a73665ba437fcd1017ab8b83
Hadise Danedaran <hdanedaran@yahoo.com>
2020-11-10 00:22:34 +0330

    add videoTag factory

---
+++ database/factories/VideoTagFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\videotag\VideoTag;
+use Faker\Generator as Faker;
+
+$factory->define(VideoTag::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
